4e704e7d4a60154e7aabb45ae6d31a7082bd3cf7
Kevin David <kevincarld@gmail.com>
2022-11-28 10:26:19 +1100

    Cleanup starter code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "custom",
+    // imageSizes: [],
+    deviceSizes: [280,360,375,428,768,1024,1280,1366,1440,1920],
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  env: {
+    //https://www.npmjs.com/package/next-image-export-optimizer
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    //
+    // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
+    // forget to set `placeholder="empty"`, you'll see 404 errors for the missing
+    // placeholder images in the console.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
 }
 
 module.exports = nextConfig
