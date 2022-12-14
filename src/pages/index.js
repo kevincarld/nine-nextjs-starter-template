4186cb7d127e1a8059343c7b68e4313c1f54bd00
Kevin David <kevincarld@gmail.com>
2022-12-14 11:55:33 +1100

    Setup starter block folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 
 import Layout from "components/Layout"
+// chakra
 import { Heading, Button } from "@chakra-ui/react"
+// blocks
+// import { Hero } from "blocks"
 
 export default function Home() {
   return (
     <Layout>
       <Heading as="h1">Hello World</Heading>
       <Button>Button</Button>
+
+      {/* <Hero /> */}
     </Layout>
   )
 }
