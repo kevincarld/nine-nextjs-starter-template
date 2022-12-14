7a7aee8b3293c74fbf4e20ebaf2e550454599683
Kevin David <kevincarld@gmail.com>
2022-12-14 14:03:23 +1100

    Move title tag to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
+        <title>{process.env.appMeta?.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
         {/* see next.config.js for setting up */}
