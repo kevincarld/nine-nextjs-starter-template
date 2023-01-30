711d31a4a7fcfd4bdec3cab4971653cd8403cdc7
Kevin David <kevincarld@gmail.com>
2023-01-30 21:41:36 +1100

    Remove useless scroll momentum

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,12 @@ import Layout from "components/util/Layout"
 // chakra
 import { Heading, Button } from "@chakra-ui/react"
 //
-import ScrollMomentum from "components/ScrollMomentum"
 
 export default function Home() {
   return (
     <Layout>
-      <ScrollMomentum>
-        <Heading as="h1">Hello World</Heading>
-        <Button>Button</Button>
-
-      </ScrollMomentum>
+      <Heading as="h1">Hello World</Heading>
+      <Button>Button</Button>
     </Layout>
   )
 }
