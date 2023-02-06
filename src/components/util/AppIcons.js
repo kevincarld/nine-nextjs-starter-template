a105d34006be5ce0d9c681110e290f48e1e16c05
Kevin David <kevincarld@gmail.com>
2023-02-06 13:56:29 +1100

    Fix 404s for good food apple touch icons

--- src/components/util/AppIcons.js
+++ src/components/util/AppIcons.js
@@ -41,32 +41,14 @@ export default function AppIcons({type="smh", theme="#0a1633"}) {
                     href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/favicon.ico" />
               <link rel="icon"
                     href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/favicon.ico" />
-              <link rel="apple-touch-icon"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="57x57"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-57x57-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="72x72"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-72x72-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="76x76"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-76x76-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="114x114"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-114x114-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="120x120"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-120x120-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="144x144"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-144x144-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-152x152-1.png" />
-              <link rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="https://brandedcontent.goodfood.com.au/wp-content/uploads/2021/03/apple-touch-icon-180x180-1.png" />
+              <link rel="apple-touch-icon" sizes="57x57" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-57x57-1.png" />
+              <link rel="apple-touch-icon" sizes="72x72" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-72x72-1.png" />
+              <link rel="apple-touch-icon" sizes="76x76" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-76x76-1.png" />
+              <link rel="apple-touch-icon" sizes="114x114" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-114x114-1.png" />
+              <link rel="apple-touch-icon" sizes="120x120" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-120x120-1.png" />
+              <link rel="apple-touch-icon" sizes="144x144" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-144x144-1.png" />
+              <link rel="apple-touch-icon" sizes="152x152" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-152x152-1.png" />
+              <link rel="apple-touch-icon" sizes="180x180" href="https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2023/02/apple-touch-icon-180x180-1.png" />
             </>
           )
           break;
