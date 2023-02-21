30cea132dbe89aa45fe603cbcd4e16a7f5086fe0
Kevin David <kevincarld@gmail.com>
2023-02-22 09:33:36 +1100

    GA4 scripts added and removed old

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -136,11 +136,6 @@ export default function App({ Component, pageProps }) {
             titan.cmd = titan.cmd || []; // TITAN BASE CONFIG END
           `}
         </Script>
-        <Script id="gtm">
-          {`
-          !function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-WV7FLW7",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");
-          `}
-        </Script>
       </>
       }
 
@@ -148,11 +143,14 @@ export default function App({ Component, pageProps }) {
 
       {!isDev && hasNineScripts &&
       <>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV7FLW7" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-101266782-31" />
-        <Script id="gta">
+        <Script id='ga4' async src="https://www.googletagmanager.com/gtag/js?id=G-8H9J6GLFEH" />
+        <Script id="ga4b">
           {`
-            window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments)} gtag("js", new Date()); gtag("config", "UA-101266782-31");
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+
+             gtag('config', 'G-8H9J6GLFEH');
           `}
         </Script>
       </>
