c77b6ddd087ce79d0b95afe8f048cd47534f0dd4
Kevin David <kevincarld@gmail.com>
2023-02-28 11:02:39 +1100

    Update slider sample

--- src/components/util/Slider/Slider.js
+++ src/components/util/Slider/Slider.js
@@ -2,54 +2,55 @@ import React from 'react'
 // chakra
 import { Box } from '@chakra-ui/react'
 //
-import { useResponsive } from 'utils/common';
 import { Image } from '@chakra-ui/react';
 // swiper
-import { Swiper, SwiperSlide } from 'swiper/react';
+import Swiper from 'swiper';
+import { SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from "swiper";
 //
 import { mock } from 'utils/common';
 
 export default function Slider({children, prevRef, nextRef, ...rest}) {
-  const isDesktop = useResponsive('up', 'd')
-  const isTablet = useResponsive('up', 'lg')
+  const slider1Ref = React.useRef(null);
+
+    // initialize sliders
+    React.useEffect(() => {
+      // Slider 1 options (cover)
+      const slider1 = new Swiper(slider1Ref.current, {
+        slidesPerView: 3, // set to negative to move invert
+        speed: 1300,
+        spaceBetween: 20,
+        modules: [Autoplay, Navigation]
+      });
+
+      return () => {
+        slider1.destroy();
+      };
+    }, [])
 
   return (
-      <Box
-        {...rest}
-        as={Swiper}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={isTablet && !isDesktop ? 2 : isDesktop ? 3 : 1}
-        loop={true}
-        spaceBetween={20}
+    <Box ref={slider1Ref} className="swiper-container slider1"  overflow='hidden'>
+      <Box className="swiper-wrapper">
+        <SwiperSlide className='swiper-slide' >
+          <Image fit='cover' h='full' w='full' src={'http://placekitten.com/640/360'} />
+        </SwiperSlide>
 
-        speed={1000}
-        autoplay={{
-          delay: 2500
-        }}
+        <SwiperSlide className='swiper-slide' >
+          <Image fit='cover' h='full' w='full' src={'http://placekitten.com/630/360'} />
+        </SwiperSlide>
 
-        modules={[Autoplay, Navigation]}
-        className="mySwiper"
-        {...(prevRef && nextRef && {
-          navigation: {
-            enabled: true,
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }
-         })}
-        {...(prevRef && nextRef && {
-          onBeforeInit: (swiper) => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-          }
-         })}
-      >
-        {[1,2,3,4,5].map(x => (
-          <SwiperSlide className='swiper-slide' key={x}>
-            <Image fit='cover' w='full' src={mock('300x500', x.toString(), 'red')} />
-          </SwiperSlide>
-        ))}
+        <SwiperSlide className='swiper-slide' >
+          <Image fit='cover' h='full' w='full' src={'http://placekitten.com/620/360'} />
+        </SwiperSlide>
+
+        <SwiperSlide className='swiper-slide' >
+          <Image fit='cover' h='full' w='full' src={'http://placekitten.com/620/360'} />
+        </SwiperSlide>
+
+        <SwiperSlide className='swiper-slide' >
+          <Image fit='cover' h='full' w='full' src={'http://placekitten.com/620/360'} />
+        </SwiperSlide>
       </Box>
+    </Box>
   )
 }
