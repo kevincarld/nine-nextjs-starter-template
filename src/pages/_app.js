46b4d0345f841524b7760d11ebf25bb1ba00074b
Kevin David <kevincarld@gmail.com>
2023-03-13 15:38:32 +1100

    Fix missing datalayer script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -139,6 +139,11 @@ export default function App({ Component, pageProps }) {
             titan.cmd = titan.cmd || []; // TITAN BASE CONFIG END
           `}
         </Script>
+        <Script id="gtm">
+          {`
+          !function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-WV7FLW7",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");
+          `}
+        </Script>
       </>
       }
 
