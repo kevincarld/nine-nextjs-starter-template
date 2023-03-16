1d6b3a28287d64a1482c4c36841d16d48c1220a5
Kevin David <kevincarld@gmail.com>
2023-03-17 10:35:16 +1100

    Added reusable animation variants

---
+++ src/utils/motion.js
@@ -0,0 +1,49 @@
+// collection of common framer motion variants
+
+export const animation_clipper = {
+  // initial
+  fromLeft: {
+    clipPath: 'polygon(0 0, 20% 0, 20% 100%, 0 100%)',
+  },
+  fromRight: {
+    clipPath: 'polygon(70% 0, 100% 0, 100% 100%, 70% 100%)',
+  },
+
+  // direction
+  toRight: {
+    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+    transition: {
+      duration: .8,
+    }
+  },
+  toLeft: {
+    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+    transition: {
+      duration: .8,
+    }
+  },
+}
+
+export const animation_shaker = {
+  shake: {
+    x: [-10, 10, -10, 10, 0],
+    transition: {
+      duration: 0.8,
+    },
+  }
+}
+
+export const animation_zoom = {
+  hide: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  zoom: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: .4,
+      duration: 1.2
+    }
+  }
+}
