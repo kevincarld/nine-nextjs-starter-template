d74e5a22df95cf1b5f3933a65b3111f44a98bc96
Kevin David <kevincarld@gmail.com>
2023-03-21 15:59:29 +1100

    Added a helper script to create react component from template

---
+++ node-scripts/create-component.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Prompt the user for the component name
+rl.question('Enter the component name: ', (componentName) => {
+  // Read the template file
+  fs.readFile('src/utils/ComponentTemplate.js', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    // Replace the placeholder component name with the user-specified name
+    const updatedData = data.replace(/ComponentTemplate/g, componentName);
+
+    // Write the updated file to the components directory with the user-specified name
+    fs.writeFile(`src/components/${componentName}.js`, updatedData, (err) => {
+      if (err) throw err;
+      console.log(`New component ${componentName} created successfully!`);
+      rl.close();
+    });
+  });
+});
---
+++ src/utils/ComponentTemplate.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, Text, Button, Link, Image, Grid, GridItem } from '@chakra-ui/react'
+import PreLine from 'components/util/PreLine'
+import NineLink from 'components/util/NineLink'
+import { mock } from 'utils/common'
+import Container from 'components/util/Container'
+import Img from 'components/util/Img'
+import Figure from 'components/util/Figure'
+
+export default function ComponentTemplate() {
+
+  return (
+    <Box>
+
+    </Box>
+  )
+}
+
+// font styles
+const texts = {
+  xx: {
+
+  }
+}
+
