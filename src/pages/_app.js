a775d1c9c2d1779fda7ec103cc59d5bac61ebb31
Kevin David <kevincarld@gmail.com>
2023-03-23 19:40:37 +1100

    Added back isDev check for ga scripts in _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,173 +36,183 @@ export default function App({ Component, pageProps }) {
       </Head>
 
        {/* // UNCOMMENT FOR: brandedcontent snow plow scripts
-      <>
-        <Script id="titan">
-          {`
-            var dataLayer = window.dataLayer || [];
+       {!isDev &&
+          <>
+            <Script id="titan">
+              {`
+                var dataLayer = window.dataLayer || [];
 
-            let pageViewId;
-            function uuidv4() {
-              return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-              );
-            }
-            pageViewId = uuidv4();
+                let pageViewId;
+                function uuidv4() {
+                  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+                    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+                  );
+                }
+                pageViewId = uuidv4();
 
-            var sysEnv = "desktop";
-            if (window.innerWidth < 1000) {
-              sysEnv = "mobile";
-            }
+                var sysEnv = "desktop";
+                if (window.innerWidth < 1000) {
+                  sysEnv = "mobile";
+                }
 
-            dataLayer.push({
-              page: {
-                author: "",
-                publishDate: "${date}AEST00:00:0000000",
-                updateDate: "${date}AEST00:00:0000000",
-                renderedPlatform: "WEB",
-                authoredPlatform: "Pantheon",
-                viewID: "",
-                type: "Article",
-                assetId: "${brand}",
-                contentSource: "${siteName}",
-                primaryTag: "",
-                tags: "",
-                components: "",
-                promotedType: "sponsored",
-                sponsored: "true",
-                name: "${brand}",
-                template: "paidcontent",
-                didMount: "false",
-                customReferrer: document.referrer,
-                lifecycleType: "single"
-              },
-              network: {
-                deviceID: sysEnv,
-                marketingID: ""
-              },
-              client: {
-                deviceType: sysEnv
-              },
-              section: {
-                primaryCategory: "brandDiscover",
-                subCategory: ""
-              },
-              session: {
-                adBlocked: "",
-                gCookie: "",
-                privateBrowsingMode: ""
-              },
-              subs: {
-                memberID: "",
-                memberType: "visitor",
-                meterCount: "",
-                meterPeriodStart: "",
-                meterPeriodEnd: ""
-              },
-              brand: {
-                name: "${siteName}",
-                Engagement: ""
-              }
-            });
-
-            // TITAN BASE CONFIG
-            var titanEnabled = true;
-            var digitalData = {
-              "page": {
-                  "pageInfo": {
-                      "sysEnv": sysEnv,
-                      "pageViewId": pageViewId
-                  }
-              },
-              "commercial": {
-                  "sponsor": "${brand}",
-                  "type": "sponsored by",
-                  "tags": ""
-              },
-              "titan": {
-                  "spa": false,
-                  "adSite": "masthead",
-                  "adZone": "SinglePageArchitechture",
-                  "adKeyValues": {
-                      "cat": "${brand}",
-                      "cat1": "${year}",
-                      "cat2": "${title}",
-                      "ctype": "immersive",
-                      "pageviewid": pageViewId
+                dataLayer.push({
+                  page: {
+                    author: "",
+                    publishDate: "${date}AEST00:00:0000000",
+                    updateDate: "${date}AEST00:00:0000000",
+                    renderedPlatform: "WEB",
+                    authoredPlatform: "Pantheon",
+                    viewID: "",
+                    type: "Article",
+                    assetId: "${brand}",
+                    contentSource: "${siteName}",
+                    primaryTag: "",
+                    tags: "",
+                    components: "",
+                    promotedType: "sponsored",
+                    sponsored: "true",
+                    name: "${brand}",
+                    template: "paidcontent",
+                    didMount: "false",
+                    customReferrer: document.referrer,
+                    lifecycleType: "single"
                   },
-                  "adSlots": ""
-              }
-            };
-            var titan = titan || {};
-            titan.cmd = titan.cmd || []; // TITAN BASE CONFIG END
-          `}
-        </Script>
-        <Script id="gtm">
-          {`
-          !function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-WV7FLW7",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");
-          `}
-        </Script>
-      </> */}
+                  network: {
+                    deviceID: sysEnv,
+                    marketingID: ""
+                  },
+                  client: {
+                    deviceType: sysEnv
+                  },
+                  section: {
+                    primaryCategory: "brandDiscover",
+                    subCategory: ""
+                  },
+                  session: {
+                    adBlocked: "",
+                    gCookie: "",
+                    privateBrowsingMode: ""
+                  },
+                  subs: {
+                    memberID: "",
+                    memberType: "visitor",
+                    meterCount: "",
+                    meterPeriodStart: "",
+                    meterPeriodEnd: ""
+                  },
+                  brand: {
+                    name: "${siteName}",
+                    Engagement: ""
+                  }
+                });
+
+                // TITAN BASE CONFIG
+                var titanEnabled = true;
+                var digitalData = {
+                  "page": {
+                      "pageInfo": {
+                          "sysEnv": sysEnv,
+                          "pageViewId": pageViewId
+                      }
+                  },
+                  "commercial": {
+                      "sponsor": "${brand}",
+                      "type": "sponsored by",
+                      "tags": ""
+                  },
+                  "titan": {
+                      "spa": false,
+                      "adSite": "masthead",
+                      "adZone": "SinglePageArchitechture",
+                      "adKeyValues": {
+                          "cat": "${brand}",
+                          "cat1": "${year}",
+                          "cat2": "${title}",
+                          "ctype": "immersive",
+                          "pageviewid": pageViewId
+                      },
+                      "adSlots": ""
+                  }
+                };
+                var titan = titan || {};
+                titan.cmd = titan.cmd || []; // TITAN BASE CONFIG END
+              `}
+            </Script>
+            <Script id="gtm">
+              {`
+              !function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-WV7FLW7",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");
+              `}
+            </Script>
+          </>
+        } */}
+
 
       {/* UNCOMMENT FOR: nine.com.au ga scripts
-      <>
-        <Script id="nineKit2" async>
-          {`
-          function runSecondScript() {
-            console.log('boom')
-            NINEAU_Loader.setup();
+       {!isDev &&
+        <>
+          <Script id="nineKit2" async>
+            {`
+            function runSecondScript() {
+              console.log('boom')
+              NINEAU_Loader.setup();
 
-            //Nine Third Party Kit - Configurationmsnportalaucatdev
-            NINEAU_Config.setOmnitureSuiteId("msnportalaucatglobal"); // change tp msnportalaucatglobal !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            NINEAU_Config.setCategory("miscellaneous"); // Page Category Name
-            NINEAU_Config.setSiteName("campaign"); // Page Site Name
-            NINEAU_Config.setSectionName("11332-lifeblood"); // Optional - Delete if unneeded.
-            NINEAU_Config.setNielsenEnabled(false);
-            NINEAU_Config.setAdcEnabled(false);
-            NINEAU_Config.setPageTrackingOnRenderTracking(true);
-            NINEAU_ThirdParty.initialize();
-            NINEAU_ThirdParty.renderTracking();
-          }
-          `}
-        </Script>
-        <Script id="nineKit1">
-          {`
-            // Nine Third Party Kit - Loader
-            (function () {
-              var LOADER_SCRIPT_URL =
-                "http://share.9cdn.net/share/short_cache/js/third_party/loaders/au.loader-latest.min.js";
-              var LOADER_SCRIPT_URL_SECURE =
-                "https://share.9cdn.net/share/short_cache/js/third_party/loaders/au.loader-latest.min.js";
-              var loaderScriptUrl =
-                window.location.protocol == "https:"
-                  ? LOADER_SCRIPT_URL_SECURE
-                  : LOADER_SCRIPT_URL;
+              //Nine Third Party Kit - Configurationmsnportalaucatdev
+              NINEAU_Config.setOmnitureSuiteId("msnportalaucatglobal"); // change tp msnportalaucatglobal !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+              NINEAU_Config.setCategory("miscellaneous"); // Page Category Name
+              NINEAU_Config.setSiteName("campaign"); // Page Site Name
+              NINEAU_Config.setSectionName("11332-lifeblood"); // Optional - Delete if unneeded.
+              NINEAU_Config.setNielsenEnabled(false);
+              NINEAU_Config.setAdcEnabled(false);
+              NINEAU_Config.setPageTrackingOnRenderTracking(true);
+              NINEAU_ThirdParty.initialize();
+              NINEAU_ThirdParty.renderTracking();
+            }
+            `}
+          </Script>
+          <Script id="nineKit1">
+            {`
+              // Nine Third Party Kit - Loader
+              (function () {
+                var LOADER_SCRIPT_URL =
+                  "http://share.9cdn.net/share/short_cache/js/third_party/loaders/au.loader-latest.min.js";
+                var LOADER_SCRIPT_URL_SECURE =
+                  "https://share.9cdn.net/share/short_cache/js/third_party/loaders/au.loader-latest.min.js";
+                var loaderScriptUrl =
+                  window.location.protocol == "https:"
+                    ? LOADER_SCRIPT_URL_SECURE
+                    : LOADER_SCRIPT_URL;
 
-              const script = document.createElement('script');
-              script.src = loaderScriptUrl;
-              script.async = true;
-              script.onload = runSecondScript;
-              document.body.appendChild(script);
-            })();
-          `}
-        </Script>
-      </> */}
+                const script = document.createElement('script');
+                script.src = loaderScriptUrl;
+                script.async = true;
+                script.onload = runSecondScript;
+                document.body.appendChild(script);
+              })();
+            `}
+          </Script>
+        </>
+      }
+    */}
 
       <Component {...pageProps} />
 
       {/* UNCOMMENT FOR: GA4 scripts
-      <>
-        <Script id='ga4' async src="https://www.googletagmanager.com/gtag/js?id=G-8H9J6GLFEH" />
-        <Script id="ga4b">
-          {`
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
+      {!isDev &&
 
-             gtag('config', 'G-8H9J6GLFEH');
-          `}
-        </Script>
-      </> */}
+        <>
+          <Script id='ga4' async src="https://www.googletagmanager.com/gtag/js?id=G-8H9J6GLFEH" />
+          <Script id="ga4b">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-8H9J6GLFEH');
+            `}
+          </Script>
+        </>
+      }
+      */}
     </React.Fragment>
   );
 }
