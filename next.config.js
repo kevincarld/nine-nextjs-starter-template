720b8ab20aa76b2f73fa59db9f608a3e33317f1e
Kevin David <kevincarld@gmail.com>
2023-04-06 16:31:23 +1000

    ensure trailing slash in url

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     deviceSizes: [360,768,1024],
     unoptimized: true,
   },
+  trailingSlash: true,
   assetPrefix: process.env.NODE_ENV === 'development' ? undefined : '.' ,
   exportPathMap: async function (
     defaultPathMap,
