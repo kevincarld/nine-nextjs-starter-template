7de728f69238e59929af1f157848afa14425f3f7
Kevin David <kevincarld@gmail.com>
2023-05-04 13:28:18 +1000

    Added default buttonstyles for round and square

--- src/theme/components/ButtonStyles.js
+++ src/theme/components/ButtonStyles.js
@@ -11,11 +11,44 @@ export const ButtonStyles = {
   sizes: {},
 
   // styles for different visual variants ("outline", "solid")
-  variants: {},
+  variants: {
+    round: {
+      bg: 'primary',
+      color: 'white',
+      fontSize: {base: '23px', desktop: '24px'},
+      lineHeight: {base: '33px', desktop:'44px'},
+      height: 'auto',
+      pt: '15px',
+      pb: '15px',
+      pl: '30px',
+      pr: '30px',
+      borderRadius: '9999px',
+
+      _hover: {
+        bg: 'white',
+        color: 'primary',
+        textDecor: 'none',
+      }
+    },
+    square: {
+      bg: 'primary',
+      color: 'white',
+      borderRadius: '0',
+      fontSize: {base: '23px', desktop: '24px'},
+      lineHeight: {base: '33px', desktop:'44px'},
+      height: 'auto',
+      py: '10px',
+      px: '45px',
+
+      _hover: {
+        bg: 'white',
+        color: 'primary'
+      }
+    },
+  },
 
   // default values for 'size', 'variant' and 'colorScheme'
   defaultProps: {
-    variant: 'solid',
-    size: "md",
+    variant: 'square',
   },
 }
